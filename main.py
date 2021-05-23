1c2c7791fd1b678fdaafe0e1c5847bcdeb95571d
lemary <69257731+lemary@users.noreply.github.com>
2021-05-23 15:12:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from alfabet import number_to_word
 import numpy as np
 
 import os
-TOKEN = os.environ["TOKEN"]
-
+#TOKEN = os.environ["TOKEN"]
+TOKEN = 1305871705:AAETizfOeuLot3R_mSx3kjClr0JA1SzHQzI
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
